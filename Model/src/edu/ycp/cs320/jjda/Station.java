ff9be216a940075587590d3d28f921fef13f471c
drew <dholtzap@ycp.edu>
2013-02-10 12:37:53 -0500

    Added basic Station framework and Test

---
+++ Model/src/edu/ycp/cs320/jjda/Station.java
@@ -0,0 +1,16 @@
+package edu.ycp.cs320.jjda;
+
+public class Station {
+
+	String name = "";
+	
+	//so far every station will at least have a name, you can easily add other parameters to the station constructor
+	//if needed in the future
+	Station(String stationName){
+		this.name = stationName;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
